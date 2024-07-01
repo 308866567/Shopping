84d30a557cf6b17f683b23763a3b1abcc4c465bd
308866567 <308866567@qq.com>
2024-07-01 20:32:34 +0800

    feign接口添加索引增删

---
+++ xbd-vip-mall/mall-api/mall-search-api/src/main/java/com/xbd/vip/mall/search/feign/SkuSearchFeign.java
@@ -0,0 +1,23 @@
+package com.xbd.vip.mall.search.feign;
+
+
+import com.xbd.mall.util.RespResult;
+import com.xbd.vip.mall.search.model.SkuEs;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+//feign接口所对应的服务的名字,对应yaml文件的服务名字
+@FeignClient(value = "mall-search")
+@RequestMapping(value = "/search")
+public interface SkuSearchFeign {
+    @PostMapping(value = "/add")
+    RespResult add(@RequestBody SkuEs skuEs);
+
+    /***
+     * 根据ID删除索引
+     * @param id
+     * @return
+     */
+    @DeleteMapping(value = "/del/{id}")
+    RespResult add(@PathVariable String id);
+}
