9c7b98fdb8519468ada95f7369d952ef01264f6d
308866567 <308866567@qq.com>
2024-07-09 16:18:14 +0800

    创建商品详情页静态生成工程

---
+++ xbd-vip-mall/mall-web/mall-page-web/src/main/java/com/xbd/vip/mall/MallWebPageApplication.java
@@ -0,0 +1,11 @@
+package com.xbd.vip.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWebPageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallWebPageApplication.class,args);
+    }
+}
