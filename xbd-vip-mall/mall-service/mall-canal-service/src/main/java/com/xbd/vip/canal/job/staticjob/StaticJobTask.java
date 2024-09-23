e6403a51d919bde0b09767d211a359759804e5c0
308866567 <308866567@qq.com>
2024-09-23 19:22:23 +0800

    静态定时任务测试

---
+++ xbd-vip-mall/mall-service/mall-canal-service/src/main/java/com/xbd/vip/canal/job/staticjob/StaticJobTask.java
@@ -0,0 +1,29 @@
+package com.xbd.vip.canal.job.staticjob;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.elasticjob.lite.annotation.ElasticSimpleJob;
+import org.springframework.stereotype.Component;
+
+/**
+ * 普通静态作业执行流程
+ * 1.指定执行周期
+ * 2.分片指定
+ * 3.指定Zookeeper中的命名空间
+ */
+@Component
+@ElasticSimpleJob(
+        jobName = "${elaticjob.zookeeper.namespace}",
+        shardingTotalCount = 1,
+        cron = "0/1 * * * * ?"//一秒一次
+)
+public class StaticJobTask implements SimpleJob {
+    /**
+     *
+     * @param shardingContext
+     */
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        System.out.println("静态执行");
+    }
+}
