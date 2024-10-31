3249773179dfe213436b95575389cd568fff3500
308866567 <93718858+308866567@users.noreply.github.com>
2024-10-31 22:08:35 +0800

    druid分页查询

--- xbd-vip-mall/mall-service/mall-dw-service/src/main/java/com/xbd/vip/mall/dw/controller/HotGoodsController.java
+++ xbd-vip-mall/mall-service/mall-dw-service/src/main/java/com/xbd/vip/mall/dw/controller/HotGoodsController.java
@@ -3,6 +3,7 @@ package com.xbd.vip.mall.dw.controller;
 import com.xbd.mall.util.RespResult;
 import com.xbd.vip.mall.dw.model.HotGoods;
 import com.xbd.vip.mall.dw.service.HotGoodsService;
+import com.xbd.vip.mall.dw.util.DruidPage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,31 @@ public class HotGoodsController {
     @Autowired
     private HotGoodsService hotGoodsService;
 
+
+    /***
+     * 分页查询
+     * localhost:8093/hot/goods/page/1/2
+     */
+    @GetMapping(value = "/page/{page}/{size}")
+    public RespResult<DruidPage<List<HotGoods>>> pageList(
+            @PathVariable(value = "page")Integer page,
+            @PathVariable(value = "size")Integer size){
+        DruidPage<List<HotGoods>> druidPage = hotGoodsService.pageList(size, page);
+        return RespResult.ok(druidPage);
+    }
+
+    /**
+     * 查询前n条
+     * localhost:8093/hot/goods/top/2
+     * @param size
+     * @return
+     */
+    @GetMapping(value = "/top/{size}")
+    public  RespResult<List<HotGoods>> topNum(@PathVariable("size") Integer size) {
+        List<HotGoods> list = hotGoodsService.topNum(size);
+        return RespResult.ok(list);
+    }
+
     /**
      * 查询所有
      * localhost:8093/hot/goods
@@ -29,14 +55,5 @@ public class HotGoodsController {
         return RespResult.ok(list);
     }
 
-    /**
-     * localhost:8093/hot/goods/top/2
-     * @param size
-     * @return
-     */
-    @GetMapping(value = "/top/{size}")
-    public  RespResult<List<HotGoods>> topNum(@PathVariable("size") Integer size) {
-        List<HotGoods> list = hotGoodsService.topNum(size);
-        return RespResult.ok(list);
-    }
+
 }
